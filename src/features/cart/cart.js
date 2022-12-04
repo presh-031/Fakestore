93478a7fdfc91a4bfa4e23ef8837cc2801e2bb89
presh-031 <akingbulugbe.dev@gmail.com>
2022-12-04 11:02:44 +0100

    still configuring global store;

---
+++ src/features/cart/cart.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: [],
+};
+
+export const counterSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1;
+    },
+    removeItem: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addItem, removeItem, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
