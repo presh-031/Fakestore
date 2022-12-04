7527eff2bb0b125375ea6ce18e29c64114038ca8
presh-031 <akingbulugbe.dev@gmail.com>
2022-12-04 08:52:40 +0100

    finished styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-
+import { FaShoppingCart } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <div>
-      <p className="border border-red-800">FakeStore</p>
-      <ul>
+    <div className="bg-black text-white py-12 px-8">
+      <h1 className="text-center text-8xl text-[#46ffd3] font-bold">FakeStore</h1>
+      <ul className="text-4xl flex items-center justify-around mt-8 border border-red-800">
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -14,8 +14,10 @@ const Navbar = () => {
         <li>
           <Link to={"/contact"}>Contact</Link>
         </li>
-        <li>
-          <Link to={"/cart"}>Cart</Link>
+        <li className="outline flex items-center justify-center">
+          <Link to={"/cart"}>
+            <FaShoppingCart />
+          </Link>
         </li>
       </ul>
     </div>
