c1c8e5ff1cf1c2c13356c360691a7fa8123118ca
presh-031 <akingbulugbe.dev@gmail.com>
2022-12-04 08:08:20 +0100

    setting up routes , installing tailwind css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import { useState } from "react";
 import axios from "axios";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div>
+      <p>FakeStore</p>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
