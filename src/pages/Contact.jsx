591ac7c918756be4cfe7ee0bbee7818fda2a7e17
presh-031 <akingbulugbe.dev@gmail.com>
2022-12-05 10:05:34 +0100

    finishedstyling contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
+import { BsGithub, BsTwitter } from "react-icons/bs";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div className="flex mt-16 justify-center gap-8">
+        {/* Github */}
+        <a href="https://github.com/presh-031">
+          <BsGithub size={48} />
+        </a>
+        {/* Twitter */}
+        <a href="https://twitter.com/Presh_031">
+          <BsTwitter size={48} />
+        </a>
+      </div>
+      <p className="text-center text-4xl font-semibold mt-16">Fakestore</p>
+      <p className="text-center text-4xl font-semibold mt-16">Bob bobbity way, Earth.</p>
+    </>
+  );
 };
 
 export default Contact;
