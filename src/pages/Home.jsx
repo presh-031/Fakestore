fdf01430b39e4235fabf6a81dec8288bb5000a10
presh-031 <akingbulugbe.dev@gmail.com>
2022-12-05 10:31:23 +0100

    styling index page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="text-center">
+      <p className="mt-16 font-medium text-2xl">BEST ONLINE STORE OF THE YEAR</p>
+      <h2 className="mt-8 font-semibold  leading-loose text-6xl">We don't do fashion, we are fashion. </h2>
+      <button className="bg-black text-white text-6xl font-bold py-4 my-4 px-8 rounded-full">Shop now</button>
+    </div>
+  );
 };
 
 export default Home;
